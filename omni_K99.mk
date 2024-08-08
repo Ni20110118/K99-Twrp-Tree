056a8c3f273d59ac80f47c85d31283d486e676db
Ni20110118 <153351536+Ni20110118@users.noreply.github.com>
2024-08-08 11:23:52 +0800

    Update omni_K99.mk

--- omni_K99.mk
+++ omni_K99.mk
@@ -8,8 +8,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from K99 device
 $(call inherit-product, device/sunritel/K99/device.mk)
